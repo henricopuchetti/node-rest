5765dfb1f393cf1a00dd4a21013296641f2ea846
Henrico Boschiero Puchetti <ricopuchetti@gmail.com>
2022-10-31 10:10:13 -0300

    Configuração do servidor e das redes

---
+++ src/app.js
@@ -0,0 +1,13 @@
+//importar as configs do servidor
+const {app,port} = require('../server')
+
+
+//rotas
+const index = require('./routes/index')(app)
+const sobre = require('./routes/sobre')(app)
+
+
+//escuta do servidor 
+app.listen(port, ()=>{
+    console.log(`Servidor escutando em http://localhost:${port}`)
+})
